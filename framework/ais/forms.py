1ea9fb8ed1fc64776fce4413bc6a52f44e63e664
Gentazz12 <gentainformatika22@gmail.com>
2024-10-08 12:10:50 +0800

    Review 5

--- framework/ais/forms.py
+++ framework/ais/forms.py
@@ -3,4 +3,4 @@ from .models import Students
 class StudentsForm(forms.ModelForm):
     class Meta:
         model = Students
-        fields = ['nim', 'name', 'email', 'phone_number', 'year', 'teacher']
+        fields = '__all__'
